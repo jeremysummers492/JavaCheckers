74f2b7d435a1be7b08541e95f7032416beb9d276
Jeremy <jeremysummers492@gmail.com>
2020-03-22 14:53:02 -0700

    Added ActionListener

--- Square.java
+++ Square.java
@@ -2,38 +2,42 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
-public class Square extends JButton{
+public class Square extends JButton implements ActionListener{
     private Color color;
     private boolean hasPiece;
     private Font font;
-    
+
     public Square(Color color){
         this.color = color;
         hasPiece = false;
-        
+
         font = new Font("Times", Font.BOLD, 40);
-        
+
         setBackground(color);
         setFont(font);
     }
-    
+
     public void addPiece(Piece p){
         hasPiece = true;
-        
+
         setLabel(p.symbol());
         setForeground(p.color());
     }
-    
+
     public void removePiece(){
         hasPiece = false;
     }
-    
+
     //return values
     public Color color(){
         return color;
     }
-    
+
     public boolean hasPiece(){
         return hasPiece;
+    }
+
+    //ActionListener
+    public void actionPerformed(ActionEvent e){
     }
 }
