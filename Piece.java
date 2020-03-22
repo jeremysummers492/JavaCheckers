c22ea3b264eb7df9b3dda603624251c7896148f6
Jeremy <jeremysummers492@gmail.com>
2020-03-22 12:53:38 -0700

    Fix headings in each class

--- Piece.java
+++ Piece.java
@@ -1,33 +1,4 @@
-
-/**
- * Write a description of class Piece here.
- *
- * @author (your name)
- * @version (a version number or a date)
- */
-public class Piece
-{
-    // instance variables - replace the example below with your own
-    private int x;
-
-    /**
-     * Constructor for objects of class Piece
-     */
-    public Piece()
-    {
-        // initialise instance variables
-        x = 0;
-    }
-
-    /**
-     * An example of a method - replace this comment with your own
-     *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
+public class Piece{
+    public Piece(){
     }
 }
